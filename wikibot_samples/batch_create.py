383a0a462c43c850e0224b7878206ad500c07735
lethosor <stahlalan19@gmail.com>
2013-07-30 14:38:41 -0400

    First draft rewrite of batch_create

--- wikibot_samples/batch_create.py
+++ wikibot_samples/batch_create.py
@@ -4,22 +4,20 @@ batch_create
 Creates a list of pages, each with the specified text.
 """
 
-from wikibot import util
+from wikibot import bot
 
-def run(user, page_list, text, edit_summary="Creating page"):
-    current = 0
-    total = len(page_list)
-    for page_name in page_list:
-        current += 1
-        util.log('Loading ' + page_name)
-        page = user.get_page(page_name)
-        if page.text != '':
-            util.log('Page has content! Skipping...')
-            continue
-        page.text = text
-        page.save(
-            bot=1,
-            summary=edit_summary + (" (%i/%i)" % (current, total))
-        )
-    util.log("Task completed.")
+class BatchCreateTask(bot.Task):
+    def __init__(self, user, page_list, text, summary='Creating page ({current}/{total})', data=None):
+        if data is None:
+            data = {}
+        super(BatchCreateTask, self).__init__(user, BatchCreateJob, data)
+        self.summary = summary
+    
+    
+
+class BatchCreateJob(bot.Job):
+    def run(self, task, page, data):
+        pass
+    
+
 
