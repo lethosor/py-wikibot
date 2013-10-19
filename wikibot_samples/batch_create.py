49494a37b6034223c02534fa5a450c0437f05854
lethosor <stahlalan19@gmail.com>
2013-10-19 09:30:20 -0400

    Allow overwriting in wikibot_samples.batch_create

--- wikibot_samples/batch_create.py
+++ wikibot_samples/batch_create.py
@@ -7,18 +7,17 @@ Creates a list of pages, each with the specified text.
 import wikibot
 
 class BatchCreateTask(wikibot.bot.Task):
-    def __init__(self, user, page_list, text, summary='Creating page ({0}/{1})', data=None):
+    def __init__(self, user, page_list, text, summary='Creating page ({0}/{1})',
+                 data=None, overwrite=False):
         if data is None:
             data = {}
         super(BatchCreateTask, self).__init__(user, BatchCreateJob, data)
-        self.text, self.summary = text, summary
+        self.text, self.summary, self.overwrite = text, summary, bool(overwrite)
         self.page_names = page_list
-    
-    
 
 class BatchCreateJob(wikibot.bot.Job):
     def run(self):
-        if self.page.text != "":
+        if self.page.text != "" and not self.task.overwrite:
             return False
         self.page.text = self.format(self.task.text)
         return True
