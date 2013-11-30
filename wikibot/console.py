9d43f99290057accb8981ab436c13c1aaabae0e8
lethosor <lethosor@gmail.com>
2013-11-29 19:45:04 -0500

    Fixes for console
    * Check for __name__ == '__main__'
    * Log out user when shell terminates
    * Prevent inclusion of empty module names
    * Catch sys.exit() and other exceptions that terminate shell
    * More descriptive error messages when imports fail

--- wikibot/console.py
+++ wikibot/console.py
@@ -8,54 +8,63 @@ import importlib
 
 import wikibot
 util = wikibot.util
-util.log('Starting interactive wikibot shell...', type='info')
 
-modules = ['sys', 'os', 're']
-for m in dir(wikibot):
-    if m not in modules and not m.startswith('__') and m not in globals():
-        modules.append('wikibot.' + m)
-
-c_vars = globals().copy()
-c_vars.update(locals())
-# Variables used below this line are not accessible in the shell
-
-args = wikibot.command_line.parse_args()
-if 'modules' in args:
+if __name__ == '__main__':
+    util.log('Starting interactive wikibot shell...', type='info')
+    
+    modules = ['sys', 'os', 're']
+    for m in dir(wikibot):
+        if m not in modules and not m.startswith('__') and m not in globals():
+            modules.append('wikibot.' + m)
+    
+    c_vars = globals().copy()
+    c_vars.update(locals())
+    # Variables used below this line are not accessible in the shell
+    
+    args = wikibot.command_line.parse_args()
+    if 'modules' in args:
+        try:
+            modules.extend(args['modules'].split(','))
+        except Exception:
+            util.log('Invalid --modules argument', type='error')
+    
+    user_exists = False
+    if 'user' in args or 'u' in args or 'login' in args:
+        try:
+            util.logf('Logging in...\r', type='progress')
+            user = wikibot.cred.get_user()
+        except ImportError:
+            wikibot.util.die('User credentials for "%s" not found' % args['user'],
+                             type='fatal')
+        user_exists = True
+        c_vars.update({'user': user})
+        wikibot.util.log("<bold>User available as 'user'")
+    
+    for m in modules:
+        name = m.split('.')[-1]
+        while name in c_vars:
+            name += '_'
+        try:
+            c_vars[name] = importlib.import_module(m)
+            if m.endswith(name):
+                util.log('Imported module "%s"%s' % (m, ' as "%s"' % name if name != m else ''),
+                         type='progress')
+            else:
+                util.log('Imported module "%s" as "%s": Scope conflict' % (m, name), type='warn')
+        except (ImportError, ValueError) as e:
+            util.log('Failed to import module "%s": %s' % (m, str(e)), type='error')
+    
+    shell = code.InteractiveConsole(c_vars)
     try:
-        modules.extend(args['modules'].split(','))
-    except Exception:
-        util.log('Invalid --modules argument', type='error')
-
-user_exists = False
-if 'user' in args or 'u' in args or 'login' in args:
-    try:
-        util.logf('Logging in...\r', type='progress')
-        user = wikibot.cred.get_user()
-    except ImportError:
-        wikibot.util.die('User credentials for "%s" not found' % args['user'],
-                         type='fatal')
-    user_exists = True
-    c_vars.update({'user': user})
-    wikibot.util.log("<bold>User available as 'user'")
-
-for m in modules:
-    name = m.split('.')[-1]
-    while name in c_vars:
-        name += '_'
-    try:
-        c_vars[name] = importlib.import_module(m)
-        if m.endswith(name):
-            util.log('Imported module "%s"%s' % (m, ' as "%s"' % name if name != m else ''),
-                     type='progress')
+        shell.interact()
+    except:
+        # Script will end anyway
+        pass
+    
+    if user_exists:
+        if user.logged_in:
+            user.logout()
         else:
-            util.log('Imported module "%s" as "%s": Scope conflict' % (m, name), type='warn')
-    except ImportError:
-        util.log('Failed to import module "%s": ImportError' % m, type='error')
-
-shell = code.InteractiveConsole(c_vars)
-shell.interact()
-if user_exists:
-    if user.logged_in:
-        user.logout()
-    else:
-        util.log('User already logged out.', type='info')
+            util.log('User already logged out.', type='info')
+else:
+    util.log('Attempted to import interactive shell!', type='warn')
